f19b86b7b8af8ea0301839bdda35e369ffa9a6b8
Abdelrahman Elsirafy <rahman2408@gmail.com>
2021-01-14 22:08:02 -0500

    commit tree eddab1c58e99e46abde056c5cf79796453b35fb1

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+require 'pry'
+
+
+
+
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    return array.map {|a| badge_maker(a)}  
+
+end
+
+def assign_rooms(array)
+    
+    
+    return array.map {|a| "Hello, #{a}! You'll be assigned to room #{array.find_index(a)+1}!"}
+end
+
+def printer(array)
+    batch_badge_creator(array).each {|badge|  puts badge}
+    assign_rooms(array).each {|room|  puts room}
+end
+printer(attendees) 
